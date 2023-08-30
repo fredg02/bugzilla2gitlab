359e438d7344836ce2fb103f97f93810f85002de
Frederic Gurr <frederic.gurr@eclipse-foundation.org>
2023-08-30 10:35:35 +0200

    Initialize a config dir if it does not exist

--- run_container.sh
+++ run_container.sh
@@ -7,4 +7,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+IFS=$'\n\t'
+SCRIPT_FOLDER="$(dirname "$(readlink -f "${0}")")"
+
+CONFIG_DIR="${SCRIPT_FOLDER}/config"
+
+if [[ ! -d "${SCRIPT_FOLDER}/config" ]]; then
+  echo "Config dir not found. Initializing..."
+  mkdir -p "${CONFIG_DIR}"
+  cp "tests/test_data/config/defaults.yml" "${CONFIG_DIR}/"
+  touch "${CONFIG_DIR}/component_mappings.yml"
+  touch "${CONFIG_DIR}/bugs"
+fi
+
 ./start_container.sh "bin/bugzilla2gitlab"
